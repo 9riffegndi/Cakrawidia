164808cb13ec006f3730e006d4456aa7ded1f49d
9riffegndi <marrief19@gmail.com>
2024-12-14 01:06:27 +0700

    a

--- src/Partials/ApplicationLogo.jsx
+++ src/Partials/ApplicationLogo.jsx
@@ -6,7 +6,7 @@ export default function ApplicationLogo({ className='' }) {
     return (
         <div  className={`md:flex hidden items-center ${className}`}>
             <Link to="/">
-                <img className={`w-[50px] ${className}`}  src="/Assets/img/logo.png" alt="Logo" />
+                <img className={`w-[50px] ${className}`}  src="/Assets/Img/logo.png" alt="Logo" />
             </Link>
             <p className="font-extrabold text-xl">Cakrawidia</p>
         </div>
