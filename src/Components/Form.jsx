01c35f28c8644231e6aad81689311e9c0d06b6bd
9riffegndi <marrief19@gmail.com>
2024-12-21 01:09:57 +0700

    add:: p-2 Form

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -3,7 +3,7 @@ import { Children } from "react"
 export default function Form({children ,onSubmit ='', className=''}) {
     return(
         <form
-            className={`flex flex-col gap-2 w-full ${className}`}
+            className={`flex flex-col gap-2 w-full p-2 ${className}`}
             onSubmit={onSubmit}
         >
             {children}
